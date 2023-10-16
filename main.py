eb2a461cb59b85885a91724bb78c12d018900dd5
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:41:28 +0400

    Basic form is done

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-first_number = float(input("What's the first number?: "))
-print("+", "-", "*", "/", sep="\n")
-operation = input("Pick an operation: ")
-second_number = float(input("What's the next number?: "))
-
-
 def add(a, b):
     return a + b
 
@@ -18,3 +12,23 @@ def multiply(a, b):
 
 def divide(a, b):
     return a / b
+
+
+def calculator(first, second, operation):
+    if operation == "+":
+        result = add(first, second)
+    elif operation == "-":
+        result = subtract(first, second)
+    elif operation == "*":
+        result = multiply(first, second)
+    elif operation == "/":
+        result = divide(first, second)
+    return result
+
+
+first_number = float(input("What's the first number?: "))
+print("+", "-", "*", "/", sep="\n")
+operation = input("Pick an operation: ")
+second_number = float(input("What's the next number?: "))
+answer = calculator(first_number, second_number, operation)
+print(answer)
