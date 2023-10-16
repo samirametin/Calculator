710a424f13aa86f6253be86993cb49a155d8493c
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:34:11 +0400

    basic operations

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ first_number = float(input("What's the first number?: "))
 print("+", "-", "*", "/", sep="\n")
 operation = input("Pick an operation: ")
 second_number = float(input("What's the next number?: "))
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
