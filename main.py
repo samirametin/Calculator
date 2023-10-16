d1f3e6c56707e51ccc2b0dced1d3f3fcf8fd5a44
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:31:18 +0400

    getting inputs for calculating

---
+++ main.py
@@ -0,0 +1,4 @@
+first_number = float(input("What's the first number?: "))
+print("+", "-", "*", "/", sep="\n")
+operation = input("Pick an operation: ")
+second_number = float(input("What's the next number?: "))
