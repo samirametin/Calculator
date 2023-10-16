4a94a02f147fdf791b4f0f16391d001454017914
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-17 00:44:51 +0400

    optimizing while loop

--- main.py
+++ main.py
@@ -17,23 +17,27 @@ def divide(a, b):
 operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
 
-is_go_on = True
-do_again = "n"
-while is_go_on:
-    if do_again == "n":
-        first_number = float(input("What's the first number?: "))
-    elif do_again == "y":
-        first_number = answer
-    elif do_again == "e":
-        print("Thanks for using our calculator! <3 <3 <3")
-        break
+def calculator_python():
+    should_go_on = True
+    first_number = float(input("What's the first number?: "))
     for key in operations:
         print(key)
-    operation_sign = input("Pick an operation: ")
-    second_number = float(input("What's the next number?: "))
-    calculator_function = operations[operation_sign]
-    answer = calculator_function(first_number, second_number)
-    print(f"{first_number} {operation_sign} {second_number} = {answer}")
-    do_again = input(
-        f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation or type 'e' for end calculation: "
-    )
+    while should_go_on:
+        operation_sign = input("Pick an operation: ")
+        second_number = float(input("What's the next number?: "))
+        calculator_function = operations[operation_sign]
+        answer = calculator_function(first_number, second_number)
+        print(f"{first_number} {operation_sign} {second_number} = {answer}")
+        do_again = input(
+            f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation or type 'e' for end calculation: "
+        )
+        if do_again == "y":
+            first_number = answer
+        elif do_again == "e":
+            should_go_on = False
+            print("Thanks for using our calculator! <3 <3 <3")
+        elif do_again == "n":
+            calculator_python()
+
+
+calculator_python()
