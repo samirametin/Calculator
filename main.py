5224f6c7e56711b258c8e06faa4a0105a2864f64
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:57:30 +0400

    adding end of calculation

--- main.py
+++ main.py
@@ -33,11 +33,14 @@ while is_go_on:
         first_number = float(input("What's the first number?: "))
     elif do_again == "y":
         first_number = answer
+    elif do_again == "e":
+        print("Thanks for using our calculator! <3 <3 <3")
+        break
     print("+", "-", "*", "/", sep="\n")
     operation = input("Pick an operation: ")
     second_number = float(input("What's the next number?: "))
     answer = calculator(first_number, second_number, operation)
     print(f"{first_number} {operation} {second_number} = {answer}")
     do_again = input(
-        f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+        f"Type 'y' to continue calculating with {answer}, type 'n' to start a new calculation or type 'e' for end calculation: "
     )
