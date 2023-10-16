7c0f5275f1e1df5e4563c17a611777b8a1faa895
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-16 23:53:41 +0400

    adding "continue with old answer or start new"

--- main.py
+++ main.py
@@ -26,9 +26,18 @@ def calculator(first, second, operation):
     return result
 
 
-first_number = float(input("What's the first number?: "))
-print("+", "-", "*", "/", sep="\n")
-operation = input("Pick an operation: ")
-second_number = float(input("What's the next number?: "))
-answer = calculator(first_number, second_number, operation)
-print(answer)
+is_go_on = True
+do_again = "n"
+while is_go_on:
+    if do_again == "n":
+        first_number = float(input("What's the first number?: "))
+    elif do_again == "y":
+        first_number = answer
+    print("+", "-", "*", "/", sep="\n")
+    operation = input("Pick an operation: ")
+    second_number = float(input("What's the next number?: "))
+    answer = calculator(first_number, second_number, operation)
+    print(f"{first_number} {operation} {second_number} = {answer}")
+    do_again = input(
+        f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+    )
